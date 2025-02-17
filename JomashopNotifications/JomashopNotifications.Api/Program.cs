cef902576b402167448da36937a984c18eae90a6
Beso Genebashvili <genebashvili99@gmail.com>
2025-02-17 23:45:36 +0400

    Added logger to API and configured request/response logging excluding Swagger

--- JomashopNotifications/JomashopNotifications.Api/Program.cs
+++ JomashopNotifications/JomashopNotifications.Api/Program.cs
@@ -1,6 +1,8 @@
 using JomashopNotifications.Api.Middleware;
 using JomashopNotifications.Application;
 using JomashopNotifications.Persistence;
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,14 +14,42 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Host.UseSerilog(
+    (context, config) => config.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.RequestPath
+                          | HttpLoggingFields.RequestHeaders
+                          | HttpLoggingFields.RequestBody
+                          | HttpLoggingFields.ResponseHeaders
+                          | HttpLoggingFields.ResponseBody;
+
+    options.RequestBodyLogLimit = 4096;
+    options.ResponseBodyLogLimit = 4096;
+
+    options.CombineLogs = true;
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Redirect("/swagger"))
-   .ExcludeFromDescription();
+   .ExcludeFromDescription()
+   .WithHttpLogging(HttpLoggingFields.None);
 
-app.MapGet("/swagger", () => Results.Redirect("/swagger"))
-   .ExcludeFromDescription();
+app.MapGet("/swagger/v1/swagger.json", () => Results.Ok())
+   .ExcludeFromDescription()
+   .WithHttpLogging(HttpLoggingFields.None);
 
+app.MapGet("/swagger/index.js", () => Results.Ok())
+   .ExcludeFromDescription()
+.WithHttpLogging(HttpLoggingFields.None);
+
+app.MapGet("/swagger/index.html", () => Results.Ok())
+   .ExcludeFromDescription()
+   .WithHttpLogging(HttpLoggingFields.None);
+
+app.UseHttpLogging();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
@@ -35,4 +65,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+try
+{
+    Log.Information("Starting application");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
