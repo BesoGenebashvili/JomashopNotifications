aa437b23460fff3b7fb93f48c906ae3b236bd63c
Beso Genebashvili <genebashvili99@gmail.com>
2025-03-17 18:03:42 +0400

    Added EmailService and corresponding EmailOptions to EventHandler project

---
+++ JomashopNotifications/JomashopNotifications.EventHandler/EmailNotifications/EmailOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace JomashopNotifications.EventHandler.EmailNotifications;
+
+public sealed record EmailOptions
+{
+    public const string SectionName = "EmailOptions";
+
+    public required string SMTPFrom { get; set; }
+    public required string Password { get; set; }
+    public string DisplayName { get; set; } = "Jomashop Notifications";
+    public required string Host { get; set; }
+    public int Port { get; set; } = 587;
+    public bool EnableSsl { get; set; } = true;
+    public string[]? BCCRecipientList { get; set; }
+}
---
+++ JomashopNotifications/JomashopNotifications.EventHandler/EmailNotifications/ProductInStockEventEmailNotificationHandler.cs
@@ -0,0 +1,14 @@
+﻿using JomashopNotifications.Application.Messages;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace JomashopNotifications.EventHandler.EmailNotifications;
+
+public sealed class ProductInStockEventEmailNotificationHandler(
+    ILogger<ProductInStockEventEmailNotificationHandler> logger) : IConsumer<ProductInStockEvent>
+{
+    public async Task Consume(ConsumeContext<ProductInStockEvent> context)
+    {
+
+    }
+}
